dc529481e5d0f4dcb6593214ae27cde02689ed0e
Faishal Khoiriansyah Wicaksono <faishal30112005@gmail.com>
2025-09-17 10:52:19 +0700

    update

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # penting untuk PWS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
