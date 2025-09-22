f5835fdef1a4715225e1c1be2287da2becd4ad12
Faishal Khoiriansyah Wicaksono <faishal30112005@gmail.com>
2025-09-22 17:21:45 +0700

    setup urls.py for main app

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
-from django.contrib import admin
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
-
+from django.urls import path
+from .views import (
+    show_main, create_product, show_product,
+    show_xml, show_json, show_xml_by_id, show_json_by_id,
+    register, login_user, logout_user
+)
 
 app_name = "main"
 
@@ -13,8 +15,7 @@ urlpatterns = [
     path("json/", show_json, name="show_json"),
     path("xml/<int:id>/", show_xml_by_id, name="show_xml_by_id"),
     path("json/<int:id>/", show_json_by_id, name="show_json_by_id"),
-    path('admin/', admin.site.urls),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
